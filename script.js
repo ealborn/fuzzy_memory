2f5d9c69f89da8b358fb1d0b9218bd6347007277
ealborn <emma.alborn@gmail.com>
2017-12-12 09:44:37 +0100

    faulty onclick reset

--- script.js
+++ script.js
@@ -2,28 +2,57 @@ const cards = ['A','A','B','B','C','C','D','D','E','E','F','F','G','G','H','H'];
 
 const fetchBoard = document.getElementById("memory_board");
 
-//calls shuffle function
-shuffle(cards);
+function resetAll() {
+  //calls shuffle function
+  shuffle(cards);
 
-//creates and displays each card div
-cards.forEach(function(card) {
-  const newElement = document.createElement('div');
-  newElement.classList.add('cardClass');
-  newElement.dataset.letter = card;
-  newElement.textContent = card;
-  fetchBoard.appendChild(newElement);
-})
+  //creates and displays each card div
+  cards.forEach(function(card) {
+    const newElement = document.createElement('div');
+    newElement.classList.add('cardClass');
+    newElement.dataset.letter = card;
+    newElement.textContent = card;
+    fetchBoard.appendChild(newElement);
+  })
 
-//shuffle function
-function shuffle(cards) {
-    let j, x, i;
-    for (i = cards.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = cards[i];
-        cards[i] = cards[j];
-        cards[j] = x;
+  //shuffle function
+  function shuffle(cards) {
+      let j, x, i;
+      for (i = cards.length - 1; i > 0; i--) {
+          j = Math.floor(Math.random() * (i + 1));
+          x = cards[i];
+          cards[i] = cards[j];
+          cards[j] = x;
+      }
     }
-  }
+}
+//resetAll();
+
+// skapa funktion som gör memoryboard = null
+//skapa sedan en funktion av hela div-loopen som inkluderar shuffle. Då börjar den tom.
+
+// //calls shuffle function
+// shuffle(cards);
+//
+// //creates and displays each card div
+// cards.forEach(function(card) {
+//   const newElement = document.createElement('div');
+//   newElement.classList.add('cardClass');
+//   newElement.dataset.letter = card;
+//   newElement.textContent = card;
+//   fetchBoard.appendChild(newElement);
+// })
+//
+// //shuffle function
+// function shuffle(cards) {
+//     let j, x, i;
+//     for (i = cards.length - 1; i > 0; i--) {
+//         j = Math.floor(Math.random() * (i + 1));
+//         x = cards[i];
+//         cards[i] = cards[j];
+//         cards[j] = x;
+//     }
+//   }
 
 //this is now an array with all the cards.
   const pickOne = document.querySelectorAll('.cardClass');
@@ -47,3 +76,8 @@ function shuffle(cards) {
     });
 
   });
+
+const restart = document.querySelector('button');
+restart.addEventListener('click', function(event) {
+  resetAll();
+});
